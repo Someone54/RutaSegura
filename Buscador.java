304f1db0aace7ed4f721fc641572d69d381eef78
Someone54 <tomagudemac@gmail.com>
2026-05-13 21:37:41 -0500

    integración del buscador

---
+++ Buscador.java
@@ -0,0 +1,30 @@
+public class Buscador{
+    public static void buscarVehiculo(ArrayList<Vehiculo> flota){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa la placa de vehiculo que quieras buscar:");
+        String placa = scanner.nextLine();
+        boolean encontrado = false;
+
+        // Recorro la flota de vehiculos
+        for(Vehiculo vehiculo : flota){
+
+            // Se compara la placa ingresada con las de los vehiculos en la flota
+            if(vehiculo.getMatricula().equalsIgnoreCase(placa)){
+                System.out.println("Vehículo encontrado:");
+                System.out.println("Placa: " + vehiculo.getMatricula());
+                System.out.println("Conductor: " + vehiculo.getConductor());
+                System.out.println("Marca: " + vehiculo.getMarca());
+                System.out.println("Tipo: " + vehiculo.getTipo());
+                System.out.println("Carga: " + vehiculo.getCarga());
+                System.out.println("Tarifa: " + vehiculo.getTarifa());
+                encontrado = true;
+                break;
+            }
+        }
+
+        // Si no se encuentra el vehiculo se le avisa al usuario
+        if(!encontrado){
+            System.out.println("Vehículo no encontrado.");
+        }
+    }
+}
